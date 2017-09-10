0ac2868c701ede5369bd042be729ff65d8c9f0c0
Faizal <conquerfu@gmail.com>
2017-09-10 21:39:09 +0800

    'register_finished'

--- register.py
+++ register.py
@@ -57,7 +57,47 @@ def usr_login():
             usr_sign_up()
 
 def usr_sign_up():
-    pass
+    # 注册子窗口，由于是创建自window的子窗口，不需要再添加至mainloop
+    window_sign_up = tk.Toplevel(window)
+    window_sign_up.geometry('350x200')
+    window_sign_up.title('Sign up window')
+
+    new_name = tk.StringVar()
+    entry_new_name = tk.Entry(window_sign_up, textvariable=new_name)
+    tk.Label(window_sign_up, text='User name: ').place(x=10, y=10)
+    entry_new_name.place(x=150, y=10)
+    
+    new_pwd = tk.StringVar()
+    tk.Label(window_sign_up, text='Password: ').place(x=10, y=50)
+    entry_usr_pwd = tk.Entry(window_sign_up, textvariable=new_pwd, show='*')
+    entry_usr_pwd.place(x=150, y=50)
+
+    new_pwd_confirm = tk.StringVar()
+    tk.Label(window_sign_up, text='Confirm password: ').place(x=10, y=90)
+    entry_usr_pwd_confirm = tk.Entry(window_sign_up, textvariable=new_pwd_confirm, show='*')
+    entry_usr_pwd_confirm.place(x=150, y=90)
+
+    def sign_to_my_website():
+        new_password = new_pwd.get()
+        new_password_confirm = new_pwd_confirm.get()
+        new_username = new_name.get()
+        with open('usrs_info.pickle', 'rb') as usr_file:
+            exist_usr_info = pickle.load(usr_file)
+        if(new_password != new_password_confirm):
+            messagebox.showerror(title='Error', message='Password and confirm password must be the same!')
+        elif(new_username in exist_usr_info):
+            messagebox.showerror(title='Error', message='The user has already signed up!')
+        else:
+            exist_usr_info[new_username] = new_password
+            with open('usrs_info.pickle', 'wb') as usr_file:
+                pickle.dump(exist_usr_info, usr_file)
+            messagebox.showinfo('Welcome', 'You have successfully signed up!')
+            window_sign_up.destroy()
+
+    btn_confirm_sign_up = tk.Button(window_sign_up, text='Sign up', command=sign_to_my_website)
+    btn_confirm_sign_up.place(x=150, y=130)
+
+
 # login and sign up button
 btn_login = tk.Button(window, text='Login', command=usr_login)
 btn_login.place(x=170, y=230)
